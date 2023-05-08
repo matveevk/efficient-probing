85800b078fca37b1c5335232822c0268a0a50fa8
Kostya Matveev <18727322+matveevk@users.noreply.github.com>
2023-05-08 17:35:55 +0300

    add embeddings retriever from bert

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 import numpy as np
 import random
 import torch
+from torch.utils.data import DataLoader 
 from transformers import BertModel, BertTokenizer, DistilBertModel, DistilBertTokenizer, PreTrainedModel, PreTrainedTokenizer
-from typing import Tuple, Type
+from typing import List, Tuple, Type
 
 
-RANDOM_STATE = 3407
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+EPS = 1e-8
+RANDOM_STATE = 3407
 
 
 def set_seed(random_state: int = RANDOM_STATE) -> None:
@@ -26,3 +28,31 @@ def get_pretrained(
     model = model_class.from_pretrained(model_name, output_hidden_states=True)
     model = model.to(device)
     return model, tokenizer
+
+
+def retrieve_embeddings_and_labels_bert(
+        dataloader: Dataloader,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+        sentence_aggregation: str = 'mean',
+        device: str = DEVICE
+    ) -> Tuple[List, List]:
+    """ Converts batches of sentences to batches of embeddings from a BERT-like model """
+    model = model.to(device).eval()
+    for texts, labels in dataloader:
+        with torch.no_grad():
+            inputs = tokenizer(texts, add_special_tokens=True, padding='longest', return_tensors='pt').to(DEVICE)
+            outputs = model(**inputs)
+            if sentence_aggregation == 'sum':
+                layer_embs = [layer_emb.sum(axis=1).squeeze().cpu().numpy() for layer_emb in outputs.hidden_states]  # for sum of embeddings
+            elif sentence_aggregation == 'cls':
+                layer_embs = [layer_emb[:, 0, :].cpu().numpy() for layer_emb in outputs.hidden_states]  # for cls token embedding
+            elif sentence_aggregation == 'mean':
+                layer_embs = []
+                for layer_emb in outputs.hidden_states:
+                    mask = inputs.attention_mask.unsqueeze(-1)  # to correctly divide the sum
+                    aggregated = torch.sum(layer_emb * mask, axis=1) / torch.clamp(mask.sum(axis=1), min=EPS)
+                    layer_embs.append(aggregated.cpu().numpy())
+            else:
+                raise NotImplementedError('not implemented sentence_aggregation {} in retrieve_embeddings_and_labels_bert'.format(sentence_aggregation))
+            yield layer_embs, labels.tolist()
